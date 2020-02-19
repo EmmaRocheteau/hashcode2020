3c9003c980d939a1f4b8d5f52c3ee27af84ca825
EmmaRocheteau <ecr38@cam.ac.uk>
2020-02-19 22:03:50 +0000

    brute force done

--- practice/brute_force.py
+++ practice/brute_force.py
@@ -1,2 +1,25 @@
-def brute_force(slices, pizza_types, pizza_slices):
-    return no_pizzas, pizzas_to_order
+import itertools
+
+def powerset(iterable):
+    '''powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)'''
+    s = list(iterable)
+    return itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s) + 1))
+
+def closest(possiblities, target):
+    return min((abs(target - total), (o_list, total))
+               for o_list, total in possiblities)[1]
+
+def bruteforce(slices, pizza_slices):
+    possiblities = []
+    for x in powerset(pizza_slices):
+        possiblities.append((x, sum(x)))
+    pizzas_to_order, score = closest(possiblities, slices)
+    no_pizzas = len(pizzas_to_order)
+    return (no_pizzas, pizzas_to_order)
+
+
+
+
+
+
+
--- practice/main.py
+++ practice/main.py
@@ -1,4 +1,4 @@
-from brute_force import brute_force
+from brute_force import bruteforce
 
 parent_dir = '/Users/emmarocheteau/PycharmProjects/hashcode2020/practice/'
 a_example = open('{}a_example.in'.format(parent_dir))
@@ -7,10 +7,13 @@ c_medium = open('{}c_medium.in'.format(parent_dir))
 d_quite_big = open('{}d_quite_big.in'.format(parent_dir))
 e_also_big = open('{}e_also_big.in'.format(parent_dir))
 
-input = a_example
+input = c_medium
 
 slices, pizza_types = input.readline().split()
 pizza_slices = [int(slices) for slices in input.readline().split()]
+
+slices = int(slices)
+pizza_types = int(pizza_types)
 
 no_pizzas = 0
 pizzas_to_order = []
@@ -18,6 +21,8 @@ pizzas_to_order = []
 # all functions should have the form:
 # def solution(slices, pizza_types, pizza_slices):
 #     return no_pizzas, pizzas_to_order
+
+no_pizzas, pizzas_to_order = bruteforce(slices, pizza_slices)
 
 output = open('{}.out'.format(input.name[:-3]), 'w')
 output.write(str(no_pizzas) + '\n')
