699725b03e5748d4c2842d525bc23ded1be9b680
EmmaRocheteau <ecr38@cam.ac.uk>
2020-02-19 21:39:18 +0000

    bleh

---
+++ practice/brute_force.py
@@ -0,0 +1,2 @@
+def brute_force(slices, pizza_types, pizza_slices):
+    return no_pizzas, pizzas_to_order
--- practice/main.py
+++ practice/main.py
@@ -1,8 +1,11 @@
-a_example = open('a_example.in')
-b_small = open('b_small.in')
-c_medium = open('c_medium.in')
-d_quite_big = open('d_quite_big.in')
-e_also_big = open('e_also_big.in')
+from brute_force import brute_force
+
+parent_dir = '/Users/emmarocheteau/PycharmProjects/hashcode2020/practice/'
+a_example = open('{}a_example.in'.format(parent_dir))
+b_small = open('{}b_small.in'.format(parent_dir))
+c_medium = open('{}c_medium.in'.format(parent_dir))
+d_quite_big = open('{}d_quite_big.in'.format(parent_dir))
+e_also_big = open('{}e_also_big.in'.format(parent_dir))
 
 input = a_example
 
@@ -12,7 +15,9 @@ pizza_slices = [int(slices) for slices in input.readline().split()]
 no_pizzas = 0
 pizzas_to_order = []
 
-# solution here
+# all functions should have the form:
+# def solution(slices, pizza_types, pizza_slices):
+#     return no_pizzas, pizzas_to_order
 
 output = open('{}.out'.format(input.name[:-3]), 'w')
 output.write(str(no_pizzas) + '\n')
