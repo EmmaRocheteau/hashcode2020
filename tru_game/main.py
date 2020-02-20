a48f071e0e920479ea3db9e7d724c98cd1120fe3
EmmaRocheteau <ecr38@cam.ac.uk>
2020-02-20 18:14:14 +0000

    addef files

---
+++ tru_game/main.py
@@ -0,0 +1,13 @@
+import sys
+from libraries import Book, Library
+
+file = open(sys.argv[1], 'r')
+
+no_books, no_libraries, no_days = file.readline().split()
+book_list = [Book(book_id, int(book_score)) for book_id, book_score in enumerate(file.readline().split())]
+
+library_list = []
+library_desc = True
+for remaining_lines in file.readlines():
+    if library_desc:
+        
